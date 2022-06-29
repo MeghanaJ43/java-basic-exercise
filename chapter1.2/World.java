be27a2f089f8a33c8d86e27a8a01ed789f51e7a5
MEGHANA.J <107172411+MeghanaJ43@users.noreply.github.com>
2022-06-29 13:12:04 +0530

    Create World.java

---
+++ chapter1.2/World.java
@@ -0,0 +1,12 @@
+//Write a program that displays Welcome to Java five times
+
+class World 
+{
+public static void main(String arg[])
+{
+for(int i=0;i<5;i++)
+{
+System.out.println("welcome to java");
+}
+}
+}
