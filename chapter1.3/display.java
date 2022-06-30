88f96cdb76386da7eba1cbc75b0991f666f2b23e
MEGHANA.J <107172411+MeghanaJ43@users.noreply.github.com>
2022-06-30 14:51:56 +0530

    Update and rename display to display.java

--- chapter1.3/display.java
+++ chapter1.3/display.java
@@ -1,8 +1,9 @@
-//Write a program that displays the following pattern:
-    J    A    V     V     A
-    J   A A    V   V     A A
-J   J  AAAAA    V V     AAAAA
- J J  A      A   V     A     A
+/*Write a program that displays the following pattern:
+         J    A    V     V     A
+        J   A A    V   V     A A
+    J   J  AAAAA    V V     AAAAA
+     J J  A      A   V     A     A*/
+ 
 class Display
 {
 public static void main(String args[])
